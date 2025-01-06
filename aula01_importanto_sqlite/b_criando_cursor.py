baa0ca30009cd679853a118f5c5bd59feaa188ef
ProfessorSebastiãoMarcos <profsebastiaomarcos@gmail.com>
2025-01-06 18:09:36 -0300

    removendo conteúdo

--- aula01_importanto_sqlite/b_criando_cursor.py
+++
@@ -1,7 +0,0 @@
-import sqlite3
-
-# Conexão com o banco de dados
-conn = sqlite3.connect("c:/projetos_sqlite3/BD/meu_banco_de_dados.db")
-# o cursosr serve para manipular o recursos do bd
-cursor = conn.cursor()
-
--- projetos_sqlite3-master/aula01_importanto_sqlite/a_importando.py
+++
@@ -1,4 +0,0 @@
-import sqlite3
-
-# Conexão com o banco de dados
-conn = sqlite3.connect("meu_banco_de_dados.db")
